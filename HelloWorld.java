1610c9963b01b4b19f472d4eae09ce0c47807512
crisuscata <crisuscata@gmail.com>
2022-12-17 17:16:35 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("** Made some changes for jenkins**");
+		System.out.println("** Made some changes for jenkins <3 **");
 	}
 }
